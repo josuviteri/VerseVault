e3fa9d86db56674454ad6a11c7e21a70d08e1b48
asier-larrazabal <asier.larrazabal@opendeusto.es>
2024-03-07 10:44:40 +0100

    Librerias para la ventana y negrita

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+// #include <glew.h>    Estas 2 librerías serán utilizadas 
+// #include <imgui.h>   para crear y gestionar las ventanas
+#define NEGRITA "\e[1m" // Renombramos el código de los caracteres en negrita para que sea mas entendible
+#define QUITAR_NEGRITA "\e[m" // Renombramos el codigo de quitar la negrita a los caracteres para que sea mas entendible
 
 int main(void)
 {
 printf("Proyecto Programacion IV\n");
-printf("prueba");
+printf(NEGRITA"Libreria virtual\n\n" QUITAR_NEGRITA);
+
 return 0;
 }
