76986ec0898c9b19a3a3b06aba1ea1e090fe05df
Potzon <dekumalditonerd@gmail.com>
2024-04-01 19:20:14 +0200

    Update function to handle edge cases

--- database/volcarLibrerias.c
+++ database/volcarLibrerias.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../sqlite/sqlite3.h"
-#include "../structs/libreria.h" 
 
-// Función para insertar datos de una librería en la base de datos
-void insertar_libreria_en_db(sqlite3 *db, Libreria libreria) {
+typedef struct Libreria {
+    char nom_libreria[100];
+    char ciudad_libreria[100];
+    char direccion_libreria[100];
+    char horario_libreria[100];
+} Libreria;
+
+void insertar_libreria_en_db(sqlite3 *db, const Libreria *libreria) {
     char *error_message = 0;
     char sql_query[500]; 
 
     // Construir la consulta SQL para insertar la librería
     sprintf(sql_query, "INSERT INTO Libreria (nom_libre, ciudad_libre, dir_libre, horario_libre) VALUES ('%s', '%s', '%s', '%s')",
-            libreria.nom_libreria, libreria.ciudad_libreria, libreria.direccion_libreria, libreria.horario_libreria);
+            libreria->nom_libreria, libreria->ciudad_libreria, libreria->direccion_libreria, libreria->horario_libreria);
 
     // Ejecutar la consulta
     int result = sqlite3_exec(db, sql_query, 0, 0, &error_message);
@@ -21,34 +26,30 @@ void insertar_libreria_en_db(sqlite3 *db, Libreria libreria) {
     }
 }
 
-// Función para insertar datos de librerías desde un archivo CSV en la base de datos
-void insertar_librerias_desde_csv_en_db(const char *nombre_archivo, sqlite3 *db) {
+void leer_librerias_desde_csv(const char *nombre_archivo, sqlite3 *db) {
     FILE *archivo = fopen(nombre_archivo, "r");
     if (archivo == NULL) {
         printf("No se pudo abrir el archivo %s\n", nombre_archivo);
         return;
     }
 
-    char linea[200]; // Asumiendo que cada línea tiene una longitud máxima de 200 caracteres
-
+    char linea[200];
     while (fgets(linea, sizeof(linea), archivo)) {
         Libreria libreria;
-
         sscanf(linea, "%[^;];%[^;];%[^;];%[^;];", 
                libreria.nom_libreria,
                libreria.ciudad_libreria,
                libreria.direccion_libreria,
                libreria.horario_libreria);
-
-        // Insertar la librería en la base de datos
-        insertar_libreria_en_db(db, libreria);
+        insertar_libreria_en_db(db, &libreria);
     }
 
     fclose(archivo);
 }
 
 int main() {
-    const char* csv2Path = "./ficheros/librerias-1.csv";
+    const char* csvPath = "./ficheros/librerias-1.csv";
+
     sqlite3 *db;
     int result = sqlite3_open("libreria.db", &db);
     if (result) {
@@ -56,11 +57,8 @@ int main() {
         return 0;
     }
 
+    leer_librerias_desde_csv(csvPath, db);
 
-    // Insertar los datos de las librerías desde el archivo CSV en la base de datos
-    insertar_librerias_desde_csv_en_db("csv2Path", db);
-
-    // Cerrar la conexión con la base de datos
     sqlite3_close(db);
 
     return 0;
